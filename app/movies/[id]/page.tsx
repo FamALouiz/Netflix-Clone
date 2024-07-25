79a87fa33668ebc610aad5e4c7727452e8eb9ad7
FamALouiz <fam@awadlouis.com>
2024-07-25 12:44:13 +0300

    Added skeleton movie by id page and functionality

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,8 +1,53 @@
 "use client";
+import axios from "axios";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function MoviePage() {
+  const [movie, setMovie] = useState<MovieData>({
+    id: 0,
+    title: "",
+    description: "",
+    videoUrl: "",
+    thumbnailUrl: "",
+    genre: "",
+    duration: 0,
+  });
   const { id } = useParams();
 
-  return <div className="text-white">{id}</div>;
+  useEffect(() => {
+    const url: string = process.env.NEXT_PUBLIC_MOVIE_URL || "";
+
+    axios
+      .get(`${url}${id}`)
+      .then((response) => {
+        const movieData: MovieData = {
+          id: response.data.id,
+          title: response.data.title,
+          description: response.data.description,
+          videoUrl: response.data.video_url,
+          thumbnailUrl: response.data.thumbnai_url,
+          genre: response.data.genre,
+          duration: response.data.duration,
+        };
+
+        setMovie(movieData);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    movie && (
+      <div className="flex justify-center items-center w-1/2">
+        <video
+          className="w-full"
+          autoPlay
+          controls
+          src={movie.videoUrl}
+          title={movie.title}
+          poster={movie.thumbnailUrl}
+        />
+      </div>
+    )
+  );
 }
