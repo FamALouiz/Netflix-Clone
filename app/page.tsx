553498aca9f47bea8b6a2f35932b86f56b392234
FamALouiz <fam@awadlouis.com>
2024-07-25 19:25:56 +0300

    Updated home page with new components

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar/Navbar";
 import Billboard from "@/components/Billboard/Billboard";
 import axios from "axios";
 import MovieCard from "@/components/MovieCard/MovieCard";
+import Trendingboard from "@/components/Trendingboard/Trendingboard";
 
 export default function Home() {
   const router = useRouter();
@@ -58,16 +59,7 @@ export default function Home() {
         </div>
 
         {/* Main content */}
-        <div className="flex flex-col w-full h-1/2 justify-start my-5 gap-4">
-          <h2 className="text-white text-3xl font-bold font-sans ml-5">
-            Trending Now
-          </h2>
-          <div className="flex justify-center gap-2 w-full">
-            {movieData.map((movie: any) => {
-              return <MovieCard key={movie.id} {...movie} />;
-            })}
-          </div>
-        </div>
+        <Trendingboard movieData={movieData} />
       </>
     )
   );
