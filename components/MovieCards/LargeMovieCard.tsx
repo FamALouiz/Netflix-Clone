b6ec0bf54d058b66e064e486c267f5a83431a183
FamALouiz <fam@awadlouis.com>
2024-07-27 16:11:03 +0300

    Added large movie card dynamic favorite loading

--- components/MovieCards/LargeMovieCard.tsx
+++ components/MovieCards/LargeMovieCard.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SlHeart, SlPlus } from "react-icons/sl";
 
 export default function LargeMovieCard(props: { movieData: MovieData }) {
   const router = useRouter();
   const { movieData } = props;
-  const [favorite, setFavorite] = useState<boolean>(true);
+  const [favorite, setFavorite] = useState<boolean>(false);
 
   const handleToggleFavorite: () => void = () => {
     const url = process.env.NEXT_PUBLIC_FAVORITES_URL || "";
@@ -35,6 +35,29 @@ export default function LargeMovieCard(props: { movieData: MovieData }) {
   const handleOnClick = () => {
     router.push(`/movies/${movieData.id}`);
   };
+
+  useEffect(() => {
+    const url = process.env.NEXT_PUBLIC_FAVORITE_URL || "";
+    const userId = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("userId"))
+      ?.split("=")[1];
+
+    if (!userId) {
+      router.push("/auth");
+      return;
+    }
+
+    axios
+      .get(`${url}${userId}?movieId=${movieData.id}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error("Error while fetching favorite");
+        }
+        setFavorite(response.data.status);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className="flex bg-neutral-700 my-3 mx-16 w-full h-full rounded-md gap-5">
